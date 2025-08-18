fbda3874c3c2b792beff527173f82f75b7301b8b
Philip Roberts <philip@philip.science>
2025-08-17 21:33:42 -0400

    add error conditions to shell script

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,11 +1,28 @@
-NIX_CONFIG="/etc/nixos"
-REPOSITORY_LOCATION=$(pwd)
-COMMIT_MESSAGE="Automated Update"
+#!/bin/bash
 
-echo "=== COPYING $NIX_CONFIG to $REPOSITORY_LOCATION ==="
-cp -r $NIX_CONFIG . || { echo "FAILURE TO COPY CONFIGURATION FILES."; exit 1; }
-echo "=== COPIED $NIX_CONFIG to $REPOSITORY_LOCATION ==="
-echo "=== PUSHING NEW NIX CONFIGURATION === "
-git add ./nixos && git commit -m "$COMMIT_MESSAGE" && git push || { echo "FAILURE TO PUSH TO REPOSITORY."; exit 1 }
-echo "=== SUCCESSFULLY PUSHED NEW NIX CONFIGURATION === "
+set -euo pipefail
+
+NIX_CONFIG="/etc/nixos"
+REPOSITORY_LOCATION="$(pwd)"
+COMMIT_MESSAGE="Automated Update"
+DEST_DIR="nixos"
+
+echo "=== COPYING ${NIX_CONFIG} to ${REPOSITORY_LOCATION}/${DEST_DIR} ==="
+
+[ -d "${DEST_DIR}" ] && rm -rf "${DEST_DIR}"
+cp -r "${NIX_CONFIG}" "${DEST_DIR}" || { echo "FAILURE TO COPY CONFIGURATION FILES."; exit 1; }
+
+echo "=== COPIED ${NIX_CONFIG} to ${REPOSITORY_LOCATION}/${DEST_DIR} ==="
+echo "=== PUSHING NEW NIX CONFIGURATION ==="
+
+{
+	git add "${DEST_DIR}" &&
+	git commit -m "${COMMIT_MESSAGE}" &&
+	git push
+} || {
+	echo "FAILURE TO PUSH TO REPOSITORY.";
+	exit 1;
+}
+
+echo "=== SUCCESSFULLY PUSHED NEW NIX CONFIGURATION ==="
 
