918e9805ad9a450f37e5060349d7baf5f5d201f8
Philip Roberts <philip@philip.science>
2025-09-14 11:27:38 -0400

    Automated Update

---
+++ researchmachine/configuration.nix
@@ -0,0 +1,153 @@
+{ config, pkgs, lib, ... }:
+
+let
+  hostName = "researchmachine";
+  enableSSH = true;
+in
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  system.stateVersion = "25.05";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # =========== System Configuration ==========
+  users.users.ph = {
+    isNormalUser = true;
+    description = "Philip Roberts";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.luks.devices."luks-d398677a-9b70-49c2-8d2f-0faa48f715fc".device = "/dev/disk/by-uuid/d398677a-9b70-49c2-8d2f-0faa48f715fc";
+
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  security.polkit.enable = true;
+
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # ========== Configuration ==========
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    networkmanager
+    tigervnc
+  ];
+
+  environment.variables = {
+    GBM_BACKEND = "nvidia-drm";
+    MOZ_DISABLE_RDD_SANDBOX = "1";
+    LIBVA_DRIVER_NAME = "nvidia";
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NVD_BACKEND = "direct";
+    GDK_SCALE = "1.5";
+    GDK_DPI_SCALE = "0.5";
+    QT_SCALE_FACTOR = "1.5";
+    XCURSOR_SIZE = "48";
+    PATH="/home/ph/.local/bin:$PATH";
+  };
+
+
+  # ========== Network Configuration ==========
+  networking.hostName = hostName;
+  networking.networkmanager.enable = true;
+  networking.firewall.allowedTCPPorts = if enableSSH then [ 22 3000 ] else [3000];
+
+  services.openssh = {
+    enable = enableSSH;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = [ "ph" ]; # Allows all users by default. Can be [ "user1" "user2" ]
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+    };
+  };
+
+  # ========== Display Server Configuration ==========
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = ["nvidia"];
+
+  services.xrdp.enable = true;
+  services.xrdp.defaultWindowManager = "sway";
+  services.xrdp.openFirewall = true;
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd \"sway --unsupported-gpu\"";
+        user = "greeter";
+      };
+    };
+  };
+
+  # ========== GPU Configuration ==========
+  hardware.nvidia = {
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+  };
+
+  # =========== Bluetooth Configuration ==========
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        FastConnectable = true;
+      };
+    };
+  };
+
+  # =========== researchmachine Configuration ==========
+  services.gitea = {
+    enable = true;
+    appName = "researchmachine Collaborate";
+    database = {
+      type = "postgres";
+    };
+    settings.server = {
+      domain = "localhost";
+      ROOT_URL = "http://localhost/";
+      HTTP_PORT = 3000;
+    };
+  };
+}
---
+++ researchmachine/home.nix
@@ -0,0 +1,243 @@
+{ config, pkgs, lib, ... }:
+
+let
+  username            = "ph";
+  enableBluetooth     = true;
+  enableDevTools      = true;
+  enableSway          = true;
+  enableGnome         = false; # Nonfunctional. write (enableSway or enableGnome) assert. Add GNOME packages.
+  enableDesktopApps   = true;
+  enableSystemApps    = true;
+  enableArchiveTools  = true;
+  enableSystemUtils   = true;
+  enableUnixTools     = true;
+
+  requiredPackages = with pkgs; [
+    brave
+    google-chrome
+    spotify-player
+    cava
+  ];
+
+  bluetooth = with pkgs; lib.optionals enableBluetooth [
+    bluez
+    blueman
+  ];
+
+  devTools = with pkgs; lib.optionals enableDevTools [
+    tmux
+    kitty
+    vscode
+    mdt
+    nodejs
+    uv
+    gnumake
+    gedit
+  ];
+
+  sway = with pkgs; lib.optionals enableSway [
+    rofi
+    wl-clipboard
+    mako
+  ];
+
+  desktopApps = with pkgs; lib.optionals enableDesktopApps [
+    libreoffice
+    discord
+    vlc
+  ];
+
+  systemApps = with pkgs; lib.optionals enableSystemApps [
+    nautilus
+    pavucontrol
+  ];
+
+  archiveTools = with pkgs; lib.optionals enableArchiveTools [
+    zip
+    xz
+    unzip
+    p7zip
+  ];
+
+  systemUtils = with pkgs; lib.optionals enableSystemUtils [
+    sysstat
+    lm_sensors # for `sensors` command
+    ethtool
+    pciutils # lspci
+    usbutils # lsusb
+  ];
+
+  unixTools = with pkgs; lib.optionals enableUnixTools [
+    htop
+    wget
+
+    ripgrep
+    jq
+    yq-go
+    fzf
+    shellcheck
+
+    # networking tools
+    dnsutils  # `dig` + `nslookup`
+    nmap # A utility for network discovery and security auditing
+    ipcalc  # it is a calculator for the IPv4/v6 addresses
+
+    # misc
+    file
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
+
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+    # productivity
+    glow # markdown previewer in terminal
+
+    btop  # replacement of htop/nmon
+    iotop # io monitoring
+    iftop # network monitoring
+
+    # system call monitoring
+    strace # system call monitoring
+    ltrace # library call monitoring
+    lsof # list open files
+  ];
+in
+{
+  # User Setup
+  home.username = "${username}";
+  home.homeDirectory = "/home/${username}";
+  home.stateVersion = "25.05";
+
+  # Packages
+  home.packages = requiredPackages
+               ++ bluetooth
+               ++ devTools
+               ++ sway
+               ++ desktopApps
+               ++ systemApps
+               ++ archiveTools
+               ++ unixTools
+               ++ systemUtils;
+  
+  services.gnome-keyring.enable = true;
+
+  wayland.windowManager.sway = {
+    enable = enableSway;
+
+    config = {
+      output = {
+        "DP-1" = {
+          resolution="1920x1080";
+          position = "0,0";
+        };
+      };
+
+      modifier = "Mod4"; # Set mod to Super key
+
+      terminal = "${pkgs.kitty}";
+      menu = "${pkgs.rofi}/bin/rofi -show drun";
+
+      keybindings = lib.mkOptionDefault {
+        "Mod4+Return" = "exec ${pkgs.kitty}/bin/kitty";
+        "Mod4+d" = "exec ${pkgs.rofi}/bin/rofi -show drun";
+        "Mod4+Shift+z" = "exec ${pkgs.brave}/bin/brave";
+      };
+
+      window = {
+        border = 0;
+        titlebar = false;
+      };
+
+      gaps = {
+        inner = 10;
+        outer = 10;
+      };
+    };
+  };
+
+  programs.vim.enable = true;
+  programs.vim.defaultEditor = true;
+
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    extraConfig = '' # used for less common options, intelligently combines if defined in multiple places.
+      # Reload Tmux config with greater ease
+      bind r source-file ~/.tmux.conf
+
+      # Switch panes with alt key
+      bind -n M-Left select-pane -L
+      bind -n M-Right select-pane -R
+      bind -n M-Up select-pane -U
+      bind -n M-Down select-pane -D
+
+      # Enable Mouse Control
+      set -g mouse on
+
+      # Status Bar
+      set -g status-left-length 20
+      set -g status-position bottom
+      set -g status-style fg=#99ffff
+      set -g status-right "#{?window_bigger,[#{window_offset_x}#,#{window_offset_y}], } %d-%b-%y | %H:%M "  
+    '';
+  };
+
+  programs.obs-studio = {
+    enable = true;
+
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi #optional AMD hardware acceleration
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Philip Roberts";
+    userEmail = "philip@philip.science";
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    #promptInit = ''PS1=\[\033]2;\h:\u:\w\007\]$PS1'';
+    shellAliases = {
+      # Basic Shell Interaction
+      l="ls -la";
+      c="clear";
+
+      # Software Development
+      g="git";
+      gs="git status";
+      t="tmux";
+      ta="tmux attach";
+      py="python3";
+
+      # NixOS Config
+      config="sudoedit /etc/nixos/configuration.nix";
+      home="sudoedit /etc/nixos/home.nix";
+      switch="sudo nixos-rebuild switch";
+      homeswitch="home && switch";
+      configswitch="config && switch";
+      validate="sudo nixos-rebuild dry-build";
+      llm_debug="cat /etc/nixos/home.nix > out.log && switch --show-trace &>> out.log";
+
+      # Everything Else
+      b="vim -R ~/.bashrc"; # Readonly now because you should be using the nix config, not editing the bashrc yourself
+      unlock_brave_profile="rm -rf .config/BraveSoftware/Brave-Browser/SingletonLock";
+    };
+  };
+}
